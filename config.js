b5885f889196c54a28891ba7f085ff8ea81d144a
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2020-10-21 09:27:19 -0500

    Fix: Fixes to deploy

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ dotenv.config({
 
 
 module.exports = {
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV || 'production',
   HOST: process.env.HOST || "127.0.0.1",
   PORT: process.env.PORT || "3000"
 }
